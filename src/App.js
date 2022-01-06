573e7b300033933a703b35ee7e1ea303e9ebd357
Vangel22 <h.vangel22@gmail.com>
2022-01-06 15:31:35 +0100

    Experimenting with MaterialUI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import {
+	Grid,
+	Card,
+	CardContent,
+	CardMedia,
+	Container,
+	Typography,
+	makeStyles,
+} from "@material-ui/core";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [data, setData] = useState([]);
+
+	const useStyles = makeStyles({
+		card: {
+			maxWidth: 345,
+			boxShadow: "0 5px 8px 0 rgba(0, 0, 0, 0.3)",
+			backgroundColor: "#fafafa",
+		},
+		media: {
+			height: 300,
+		},
+	});
+
+	useEffect(() => {
+		fetch("https://finalspaceapi.com/api/v0/character/?limit=12")
+			.then((res) => res.json())
+			.then((data) => setData(data));
+	}, []);
+
+	const styling = useStyles();
+
+	return (
+		<Container className="containerStyle">
+			<Typography color="textPrimary" gutterBottom variant="h2" align="center">
+				React Material UI Example
+			</Typography>
+			<Grid container spacing={3}>
+				{data.map((character) => (
+					<Grid item xs={12} sm={4} key={character.id}>
+						<Card className={styling.card}>
+							<CardMedia className={styling.media} image={character.img_url} />
+							<CardContent>
+								<Typography color="primary" variant="h5">
+									{character.name}
+								</Typography>
+								<Typography color="secondary" variant="subtitle2">
+									{character.status}
+								</Typography>
+							</CardContent>
+						</Card>
+					</Grid>
+				))}
+			</Grid>
+		</Container>
+	);
 }
 
 export default App;
